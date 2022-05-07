fbf6646f0316d1d6a51f2cb5e2c9286690553619
snacksident <andy.m.olsen@gmail.com>
2022-05-07 11:45:06 -0700

    solve countingSort problem

--- solutions.js
+++ solutions.js
@@ -123,9 +123,31 @@ function lonelyinteger(a) {
 // lonelyinteger([1,2,3,4,3,2,1])
 
 function diagonalDifference(arr){
-    for(let i = 0; i < arr.length; i++){
-        console.log(arr[i])
+    const len = arr.length
+    let diag1 = 0
+    let diag2 = 0
+    for(let i = 0; i < len; i++){
+        for(let j = 0; j < len; j++){
+            if(i===j){
+                diag1 += arr[i][j]
+            }
+
+            if(i + j === len - 1){
+                diag2 += arr[i][j]
+            }
+        }
     }
+    return Math.abs(diag1 - diag2)
 }
 
-diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ])
+// diagonalDifference([ [ 11, 2, 4 ], [ 4, 5, 6 ], [ 10, 8, -12 ] ])
+
+function countingSort(arr) {
+    // Write your code here
+    let returnArr = Array(100).fill(0)
+    for(let i = 0; i < arr.length; i++){
+        returnArr[arr[i]] ++
+    }
+    console.log(returnArr)
+}
+countingSort("63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33".split(' '))
