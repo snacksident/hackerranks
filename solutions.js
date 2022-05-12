2581a9c73edc5e79439a64f233c9919b3dc92008
snacksident <andy.m.olsen@gmail.com>
2022-05-12 09:59:38 -0700

    add pickingNumbers to solutions

--- solutions.js
+++ solutions.js
@@ -152,3 +152,31 @@ function countingSort(arr) {
 }
 // countingSort("63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33".split(' '))
 
+function gradingStudents(grades) {
+    // console.log(grades)
+    //if grade is below 40, do nothing
+    //if 2nd digit is less than 5 and greater than 3, change digit to 5.
+    //if 2nd digit is 8 or 9, change to 0 and increase first digit by 1.
+    grades.forEach(grade=>{
+        let splitGrade = grade.toString().split('')
+        if(grade < 40){
+            //do nothing
+        }else if(splitGrade[1] < 5 && splitGrade[1] >=3){
+            console.log(`found one that needs to be changed: ${grade}`)
+            //change strings back to ints, do the math
+            splitGrade[1] = 5
+            console.log(grade)
+        }else if(splitGrade[1] <= 9 && splitGrade[1] >=8){
+            console.log(`found one that needs to be changed: ${grade}`)
+        }
+        console.log(splitGrade.join())
+    })
+}
+
+// gradingStudents([73,67,38,33])
+
+function pickingNumbers(a){
+    console.log(a)
+}
+
+pickingNumbers([ 4, 6, 5, 3, 3, 1 ])
